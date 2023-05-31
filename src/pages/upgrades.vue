380086669d77567bd71b520f62373a999603fa6e
David Flowers II <master4491@gmail.com>
2023-05-30 23:02:17 -0500

    Add files via upload

---
+++ src/pages/upgrades.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page>
+    <h3>You have rescued {{ opossums }} opossums!</h3>
+    <img :src="sillyGuys[1]" />
+    <button @click="opossums = opossums + 1">Rescue an Opossum</button>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+  data() {
+    return {
+      opossums: 0,
+      sillyGuys: ["/images/silly.jpeg", "/images/silly2.jpeg"],
+    };
+  },
+});
+</script>
