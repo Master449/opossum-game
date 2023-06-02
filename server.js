1a0435a2656ce04deda62231f16729e69d7b6946
David Flowers <master4491@gmail.com>
2023-06-01 20:41:53 -0500

    project restructure

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path');
 
+
 const app = express();
+const port = 3000
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -19,3 +22,17 @@ app.set('view engine', 'html');
 
 // Static Folders (CSS + Vue Components)
 app.use(express.static(path.join(__dirname, 'assets')));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+
+// 404 Catch-all
+app.get('*', (req, res) => {
+    console.log("404: User Tried Requesting -> " + req.url)
+    res.sendFile(__dirname + "/views/404.html");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
